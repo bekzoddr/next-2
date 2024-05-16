3379f21fe91fdae2142f11c4318d022904155d2b
bekzoddr <149781755+bekzoddr@users.noreply.github.com>
2024-05-17 00:17:54 +0500

    contact\

--- src/components/form/form.jsx
+++ src/components/form/form.jsx
@@ -2,21 +2,21 @@ import React from "react";
 import Button from "@mui/material/Button";
 import Image from "next/image";
 import image from "@/assets/images/formm.png";
+import { Container } from "@mui/material";
 const Form = () => {
   return (
-    // <div className="formm">
-    // <Image src={image} />
-    <div className="form" style={{ backgroundImage: `url(${image})` }}>
-      <div className="title">
-        <Button variant="contained">New laptop</Button>
-        <div className="caption">
-          <h2>Sale up to 50% off</h2>
-          <h4>12 inch hd display</h4>
+    <Container maxWidth="xl">
+      <div className="form" style={{ backgroundImage: `url(${image})` }}>
+        <div className="title">
+          <Button variant="contained">New laptop</Button>
+          <div className="caption">
+            <h2>Sale up to 50% off</h2>
+            <h4>12 inch hd display</h4>
+          </div>
+          <Button variant="contained">Shop now</Button>
         </div>
-        <Button variant="contained">Shop now</Button>
       </div>
-    </div>
-    // </div>
+    </Container>
   );
 };
 
